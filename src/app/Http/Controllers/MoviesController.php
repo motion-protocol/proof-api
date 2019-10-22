1beebab2207eb1f6ca11d369ce651de1da34cdc8
mohamed <mohamed.b@bookatiger.com>
2019-10-22 14:41:43 +0200

    Create the http endpoints

---
+++ src/app/Http/Controllers/MoviesController.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use ProofRegistry\Application\Movie\AddRightHolderCommand;
+use ProofRegistry\Application\Movie\MovieApplicationService;
+use ProofRegistry\Application\Movie\MovieOfImdbIdQuery;
+use ProofRegistry\Application\Movie\MovieRightsHoldersQuery;
+use ProofRegistry\Application\Movie\NewMovieCommand;
+
+class MoviesController extends Controller
+{
+    /**
+     * @var MovieApplicationService
+     */
+    private $movieApplicationService;
+
+    /**
+     * MoviesController constructor.
+     * @param MovieApplicationService $movieApplicationService
+     */
+    public function __construct(MovieApplicationService $movieApplicationService)
+    {
+        $this->movieApplicationService = $movieApplicationService;
+    }
+
+    public function newMovie(Request $request, string $imdbId)
+    {
+        $tokenId = $request->get('tokenId');
+        $command = new NewMovieCommand($imdbId, $tokenId);
+        $this->movieApplicationService->newMovie($command);
+    }
+
+    public function getMovieInfo(string $imdbId)
+    {
+        $query = new MovieOfImdbIdQuery($imdbId);
+        $movieDTO = $this->movieApplicationService->movieOfImdbId($query);
+
+        return response()->json($movieDTO);
+    }
+
+    public function addRightsHolder(Request $request, string $tokenId)
+    {
+        $address = $request->get('address');
+        $name = $request->get('name');
+        $amount = $request->get('amount');
+        $command = new AddRightHolderCommand($tokenId, $address, $name, $amount);
+
+        $this->movieApplicationService->addRightsHolder($command);
+    }
+
+    public function listRightsHolders(string $tokenId)
+    {
+        $query = new MovieRightsHoldersQuery($tokenId);
+        $rightsHoldersDTO = $this->movieApplicationService->movieRightsHolders($query);
+
+        return response()->json($rightsHoldersDTO);
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/movies/{imdbId}', 'MoviesController@newMovie');
+Route::get('/movies/{imdbId}', 'MoviesController@getMovieInfo');
+Route::post('/tokens/{tokenId}', 'MoviesController@addRightsHolder');
+Route::get('/tokens/{tokenId}', 'MoviesController@listRightsHolders');
